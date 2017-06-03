82d5ec3f4aed18452edfdbf0916a19aac5ce214f
Reese Walton <jrwalt4@gmail.com>
2017-06-03 01:27:48 -0400

    add react-router and react-router-redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,52 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { createStore, Store, Reducer } from 'redux';
+import { compose, combineReducers, createStore, Store, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import { Route } from 'react-router-dom';
+import { ConnectedRouter, routerReducer, routerMiddleware, RouterState } from 'react-router-redux';
+
+import createBrowserHistory from 'history/createBrowserHistory';
 
 import { App } from './components/App/App';
 import { reducer } from './reducers/root-reducer';
 import { RootState } from './reducers/root-state';
 import './index.css';
 
-let store: Store<RootState> = createStore(reducer as Reducer<RootState>);
+export type CombinedState = {
+  state: RootState,
+  router: RouterState
+};
+
+let history = createBrowserHistory();
+
+interface Window {
+  __REDUX_DEVTOOLS_EXTENSION__: typeof applyMiddleware;
+  __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: typeof compose;
+}
+
+declare let window: Window;
+let composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store: Store<CombinedState> = createStore<CombinedState>(
+  combineReducers<CombinedState>({
+    state: reducer,
+    router: routerReducer
+  }),
+  composeEnhancers(
+    applyMiddleware(
+      routerMiddleware(history)
+    )
+  )
+);
 
 ReactDOM.render(
   (
     <Provider store={store}>
-      <App />
+      <ConnectedRouter history={history}>
+        <div>
+          <Route path="*" component={App} />
+        </div>
+      </ConnectedRouter>
     </Provider>
   ),
   document.getElementById('root') as HTMLElement
