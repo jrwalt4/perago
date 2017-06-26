f51e9c5a610392d2400dfc01f250e5c9f9540139
Reese Walton <Reese.Walton@stantec.com>
2017-06-26 13:51:00 -0400

    Header and Footer

--- src/components/containers/Footer/index.tsx
+++ src/components/containers/Footer/index.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
 export let Footer = () => (
   <div className="navbar fixed-bottom bg-faded">
-    Footer
+    <ul className="nav">
+      <li className="nav-item"><Link className="nav-link" to="timecard">Timecard</Link></li>
+      <li className="nav-item"><Link className="nav-link" to="schedule">Schedule</Link></li>
+      <li className="nav-item"><Link className="nav-link" to="database">Database</Link></li>
+    </ul>
   </div>
 );
--- src/components/containers/Header/index.tsx
+++ src/components/containers/Header/index.tsx
@@ -5,9 +5,10 @@ import './Header.css';
 export let Header = () => (
   <div className="Header navbar sticky-top bg-primary">
     <ul className="nav">
-      <li className="nav-item"><a className="nav-link" href="#">Home</a></li>
-      <li className="nav-item"><a className="nav-link" href="#">About</a></li>
-      <li className="nav-item"><a className="nav-link" href="#">Help</a></li>
+      <li className="nav-item"><a className="nav-link">File</a></li>
+      <li className="nav-item"><a className="nav-link">Edit</a></li>
+      <li className="nav-item"><a className="nav-link">View</a></li>
+      <li className="nav-item"><a className="nav-link">Help</a></li>
     </ul>
   </div>
 );
