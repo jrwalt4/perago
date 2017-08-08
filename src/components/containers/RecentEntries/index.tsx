592a3475583a5027ef21ed3b0eb5c54b0cd13b1a
Walton, Reese <jrwalt4@gmail.com>
2017-08-08 17:28:08 -0400

    Start Task from RecentEntries

--- src/components/containers/RecentEntries/index.tsx
+++ src/components/containers/RecentEntries/index.tsx
@@ -2,26 +2,37 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 
 import { PgTask } from '../../../store/models';
+import { startTask } from '../../../store/actions';
 import { PgAppState } from '../../../store';
 
 import './RecentEntries.css';
 
 type RecentEntryProps = {
-  recentTasks: PgTask[]
+  recentTasks: PgTask[];
+  continueTask: (taskId: string) => void;
 };
 
-let RecentEntriesComponent = ({ recentTasks }: RecentEntryProps) => (
+let RecentEntriesComponent = ({ recentTasks, continueTask }: RecentEntryProps) => (
   <ul className="RecentEntries">
     {
       recentTasks.map((task) => (
-        <li key={task._id}>{task.name || 'Unknown'}</li>
+        <li key={task._id}>
+          <span>{task.name || 'Unknown'}</span>
+          <span className="RecentEntries-controls fa fa-retweet" onClick={() => continueTask(task._id)} />
+        </li>
       ))
     }
   </ul>
 );
 
-export let RecentEntries = connect(({ model, view }: PgAppState) => {
-  return {
-    recentTasks: view.recentTasks.map((taskId) => model.tasks.get(taskId))
-  };
-})(RecentEntriesComponent);
+export let RecentEntries = connect(
+  ({ model, view }: PgAppState) => {
+    return {
+      recentTasks: view.recentTasks.map((taskId) => model.tasks.get(taskId))
+    };
+  },
+  (dispatch) => ({
+    continueTask: (taskId: string) => {
+      dispatch(startTask(taskId));
+    }
+  }))(RecentEntriesComponent);
